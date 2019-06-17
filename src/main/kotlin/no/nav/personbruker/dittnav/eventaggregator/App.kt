38f9ff08fe0d90720f5b72a06cf025efa718569b
Jørn Greger Nyegaard-Larsen <jorn.greger.nyegaard-larsen@nav.no>
2019-06-17 09:49:52 +0200

    * Appen er nå satt opp i Vault, og det er lagt til en dummy secret som leses inn av appen for å demonstrere at dette fungerer. * Det er også demonstrert at ikke sensitive properties definert i nais.yaml kan leses inn av appen. * Byttet til ny ingress under dev-sbs.nais.io.

--- src/main/kotlin/no/nav/personbruker/dittnav/eventaggregator/App.kt
+++ src/main/kotlin/no/nav/personbruker/dittnav/eventaggregator/App.kt
@@ -7,12 +7,33 @@ fun main(args: Array<String>) {
 
     runBlocking {
         Server.startServer(System.getenv("PORT")?.toInt() ?: 8080).start()
+        lesInnProperties()
 
-        Consumer.apply {
-            create(topics = listOf("example.topic.dittnav.informasjon"), kafkaProps = Config.consumerProps(Environment()))
-            fetchFromKafka<Informasjon>()
-        }
-
+        startConsumer()
     }
 
+}
+
+private fun lesInnProperties() {
+    lesInnSecret()
+    lesInnIkkeSensitivPropertyDefinertINaisManifiestet()
+}
+
+private fun lesInnSecret() {
+    val key = "keyForSecret"
+    val dummySecretValue: String = System.getenv(key) ?: "ikke_funnet"
+    println("Leste inn miljøvariabelen 'keyForSecret' til å være: $dummySecretValue")
+}
+
+private fun lesInnIkkeSensitivPropertyDefinertINaisManifiestet() {
+    val key = "IKKE_SENSITIV_PROPERTY"
+    val propertyValue: String = System.getenv(key) ?: "ikke_funnet"
+    println("Leste inn miljøvariabelen IKKE_SENSITIV_PROPERTY til å være: $propertyValue")
+}
+
+private fun startConsumer() {
+    Consumer.apply {
+        create(topics = listOf("example.topic.dittnav.informasjon"), kafkaProps = Config.consumerProps(Environment()))
+        fetchFromKafka<Informasjon>()
+    }
 }
