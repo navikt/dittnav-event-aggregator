e3af1273bb10772c1c3500e9f4c31968b249cd60
Torbjørn Tessem <torbjorn.tessem@nav.no>
2019-09-24 14:31:06 +0200

    [PB-186] Tilpasset OppgaveQueriesTest til å bruke junit5

--- src/main/resources/db/migration/V2__create_oppgave_table.sql
+++ src/main/resources/db/migration/V2__create_oppgave_table.sql
@@ -1,13 +1,14 @@
-create TABLE OPPGAVE (
-                         id              serial primary key,
-                         produsent       varchar(100),
-                         eventTidspunkt  timestamp,
-                         aktorid         varchar(50),
-                         eventId         varchar(50),
-                         dokumentId      varchar(100),
-                         tekst           varchar(500),
-                         link            varchar(200),
-                         sikkerhetsnivaa integer,
-                         sistOppdatert   timestamp,
-                         aktiv           boolean
+create TABLE OPPGAVE
+(
+    id              serial primary key,
+    produsent       varchar(100),
+    eventTidspunkt  timestamp,
+    aktorid         varchar(50),
+    eventId         varchar(50),
+    dokumentId      varchar(100),
+    tekst           varchar(500),
+    link            varchar(200),
+    sikkerhetsnivaa integer,
+    sistOppdatert   timestamp,
+    aktiv           boolean
 )
--- src/test/kotlin/no/nav/personbruker/dittnav/eventaggregator/entity/objectmother/OppgaveObjectMother.kt
+++ src/test/kotlin/no/nav/personbruker/dittnav/eventaggregator/entity/objectmother/OppgaveObjectMother.kt
@@ -5,22 +5,6 @@ import java.time.ZoneId
 
 object OppgaveObjectMother {
 
-    fun createOppgave(i: Int): Oppgave {
-        return Oppgave(
-                id = i,
-                produsent = "DittNav",
-                eventTidspunkt = LocalDateTime.now(ZoneId.of("Europe/Oslo")),
-                aktorId = "12345",
-                eventId = i.toString(),
-                dokumentId = "Dok12345",
-                tekst = "Dette er en oppgave til brukeren",
-                link = "https://nav.no/systemX/$i",
-                sikkerhetsinvaa = 4,
-                sistOppdatert = LocalDateTime.now(ZoneId.of("Europe/Oslo")),
-                aktiv = true
-        )
-    }
-
     fun createOppgave(i: Int, aktorId: String): Oppgave {
         return Oppgave(
                 id = i,
