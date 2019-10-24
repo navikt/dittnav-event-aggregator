c150dd01aa05ab712e6ab5943cf947be1633b4f4
Jørn Greger Nyegaard-Larsen <jorn.greger.nyegaard-larsen@nav.no>
2019-10-24 11:14:04 +0200

    * Lagt til kjøring av Flyway som en del av appens startup-hook. * Flyttet ut definisjonen av metrics-endepunktet til filen for helse-API.
    PB-199. Forenkle lokal testing med sikkerhet aktivert

--- src/main/kotlin/no/nav/personbruker/dittnav/eventaggregator/common/api/health.kt
+++ src/main/kotlin/no/nav/personbruker/dittnav/eventaggregator/common/api/health.kt
@@ -4,8 +4,11 @@ import io.ktor.application.call
 import io.ktor.http.ContentType
 import io.ktor.http.HttpStatusCode
 import io.ktor.response.respondText
+import io.ktor.response.respondTextWriter
 import io.ktor.routing.Routing
 import io.ktor.routing.get
+import io.prometheus.client.CollectorRegistry
+import io.prometheus.client.exporter.common.TextFormat
 import no.nav.personbruker.dittnav.eventaggregator.config.ApplicationContext
 
 fun Routing.healthApi(appContext: ApplicationContext) {
@@ -19,6 +22,13 @@ fun Routing.healthApi(appContext: ApplicationContext) {
             call.respondText(text = "READY", contentType = ContentType.Text.Plain)
         } else {
             call.respondText(text = "NOTREADY", contentType = ContentType.Text.Plain, status = HttpStatusCode.FailedDependency)
+        }
+    }
+
+    get("/metrics") {
+        val names = call.request.queryParameters.getAll("name")?.toSet() ?: emptySet()
+        call.respondTextWriter(ContentType.parse(TextFormat.CONTENT_TYPE_004), HttpStatusCode.OK) {
+            TextFormat.write004(this, CollectorRegistry.defaultRegistry.filteredMetricFamilySamples(names))
         }
     }
 
--- src/main/kotlin/no/nav/personbruker/dittnav/eventaggregator/config/Bootstrap.kt
+++ src/main/kotlin/no/nav/personbruker/dittnav/eventaggregator/config/Bootstrap.kt
@@ -1,14 +1,11 @@
 package no.nav.personbruker.dittnav.eventaggregator.config
 
-import io.ktor.application.*
+import io.ktor.application.Application
+import io.ktor.application.ApplicationStarted
+import io.ktor.application.ApplicationStopPreparing
+import io.ktor.application.install
 import io.ktor.features.DefaultHeaders
-import io.ktor.http.ContentType
-import io.ktor.http.HttpStatusCode
-import io.ktor.response.respondTextWriter
-import io.ktor.routing.get
 import io.ktor.routing.routing
-import io.prometheus.client.CollectorRegistry
-import io.prometheus.client.exporter.common.TextFormat
 import io.prometheus.client.hotspot.DefaultExports
 import no.nav.personbruker.dittnav.eventaggregator.common.api.healthApi
 import no.nav.personbruker.dittnav.eventaggregator.done.doneEventsApi
@@ -25,12 +22,7 @@ fun Application.mainModule(appContext: ApplicationContext = ApplicationContext()
         informasjonEventsApi(appContext.informasjonProducer)
         oppgaveEventsApi(appContext.oppgaveProducer)
         meldingEventsApi(appContext.meldingProducer)
-        get("/metrics") {
-            val names = call.request.queryParameters.getAll("name")?.toSet() ?: emptySet()
-            call.respondTextWriter(ContentType.parse(TextFormat.CONTENT_TYPE_004), HttpStatusCode.OK) {
-                TextFormat.write004(this, CollectorRegistry.defaultRegistry.filteredMetricFamilySamples(names))
-            }
-        }
+        configureShutdownHook(appContext)
 
         configureStartupHook(appContext)
         configureShutdownHook(appContext)
@@ -41,6 +33,7 @@ fun Application.mainModule(appContext: ApplicationContext = ApplicationContext()
 
 private fun Application.configureStartupHook(appContext: ApplicationContext) {
     environment.monitor.subscribe(ApplicationStarted) {
+        Flyway.runFlywayMigrations(appContext.environment)
         KafkaConsumerSetup.startAllKafkaPollers(appContext)
         appContext.cachedDoneEventConsumer.poll()
     }
