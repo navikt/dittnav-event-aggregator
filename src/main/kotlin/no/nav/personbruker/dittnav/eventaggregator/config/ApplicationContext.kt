b3981a2bb315980a800b7983e6016f269a04ffcf
Jørn Greger Nyegaard-Larsen <jorn.greger.nyegaard-larsen@nav.no>
2019-11-05 15:14:08 +0100

    Fjernet rest-endepunktene som tidligere har blitt brukt til testing. Man kan nå heller bruke appen dittnav-event-tests-producer til slik testing.
    PB-255. Egen app for å produsere kafka-eventer i preprod

--- src/main/kotlin/no/nav/personbruker/dittnav/eventaggregator/config/ApplicationContext.kt
+++ src/main/kotlin/no/nav/personbruker/dittnav/eventaggregator/config/ApplicationContext.kt
@@ -3,10 +3,6 @@ package no.nav.personbruker.dittnav.eventaggregator.config
 import no.nav.personbruker.dittnav.eventaggregator.common.database.Database
 import no.nav.personbruker.dittnav.eventaggregator.common.database.PostgresDatabase
 import no.nav.personbruker.dittnav.eventaggregator.done.CachedDoneEventConsumer
-import no.nav.personbruker.dittnav.eventaggregator.done.DoneProducer
-import no.nav.personbruker.dittnav.eventaggregator.informasjon.InformasjonProducer
-import no.nav.personbruker.dittnav.eventaggregator.melding.MeldingProducer
-import no.nav.personbruker.dittnav.eventaggregator.oppgave.OppgaveProducer
 
 class ApplicationContext {
 
@@ -19,10 +15,4 @@ class ApplicationContext {
     val doneConsumer = KafkaConsumerSetup.setupConsumerForTheDoneTopic(environment, database)
 
     val cachedDoneEventConsumer = CachedDoneEventConsumer(database)
-
-    val informasjonProducer = InformasjonProducer
-    val oppgaveProducer = OppgaveProducer
-    val meldingProducer = MeldingProducer
-    val doneProducer = DoneProducer
-
 }
