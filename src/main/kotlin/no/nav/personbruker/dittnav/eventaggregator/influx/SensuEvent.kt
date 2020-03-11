b2378290661d26b39fe367cffffbf2361d3605ea
Christopher Santana <christopher.santana@nav.no>
2020-03-11 10:58:08 +0100

    Fikset manglende krøllbrakke.

--- src/main/kotlin/no/nav/personbruker/dittnav/eventaggregator/influx/SensuEvent.kt
+++ src/main/kotlin/no/nav/personbruker/dittnav/eventaggregator/influx/SensuEvent.kt
@@ -14,6 +14,7 @@ data class SensuEvent(
                 "handlers": [ "events_nano" ],
                 "status": 0,
                 "output": "${dataPoint.lineProtocol()}"
+            }
         """.trimIndent()
     }
 }
