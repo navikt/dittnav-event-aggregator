7c4a6bead61e704ce76657f51a46877a2ef1af9d
Jørn Greger Nyegaard-Larsen <jorn.greger.nyegaard-larsen@nav.no>
2020-03-25 16:44:22 +0100

    Fjerner det midlertidige ytest-prefix-et.
    PB-138. Ytelsestesting av DEA

--- src/main/resources/db/migration/V1.0.1__Legger_til_aktiv_i_brukernotifikasjon-view-et.sql
+++ src/main/resources/db/migration/V1.0.1__Legger_til_aktiv_i_brukernotifikasjon-view-et.sql
@@ -1,6 +1,6 @@
-CREATE OR REPLACE VIEW ytest_brukernotifikasjon_view AS
-SELECT eventId, produsent, 'beskjed' as type, fodselsnummer, aktiv FROM YTEST_BESKJED
+CREATE OR REPLACE VIEW brukernotifikasjon_view AS
+SELECT eventId, produsent, 'beskjed' as type, fodselsnummer, aktiv FROM BESKJED
 UNION
-SELECT eventId, produsent, 'oppgave' as type, fodselsnummer, aktiv FROM YTEST_OPPGAVE
+SELECT eventId, produsent, 'oppgave' as type, fodselsnummer, aktiv FROM OPPGAVE
 UNION
-SELECT eventId, produsent, 'innboks' as type, fodselsnummer, aktiv FROM YTEST_INNBOKS;
+SELECT eventId, produsent, 'innboks' as type, fodselsnummer, aktiv FROM INNBOKS;
