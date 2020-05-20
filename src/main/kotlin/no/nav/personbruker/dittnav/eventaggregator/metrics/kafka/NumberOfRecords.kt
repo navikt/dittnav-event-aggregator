d3f7f479313cb2e3eb3f88844d71f773df0623e3
Jørn Greger Nyegaard-Larsen <jorn.greger.nyegaard-larsen@nav.no>
2020-05-20 16:14:55 +0200

    * Rørget for at totalen kun blir beregnet en gang.
    PB-438. Sammenligne antall kafka-eventer mot rader i cache-en

--- src/main/kotlin/no/nav/personbruker/dittnav/eventaggregator/metrics/kafka/NumberOfRecords.kt
+++ src/main/kotlin/no/nav/personbruker/dittnav/eventaggregator/metrics/kafka/NumberOfRecords.kt
@@ -4,8 +4,10 @@ data class NumberOfRecords(val beskjed: Long = 0,
                            val innboks: Long = 0,
                            val oppgaver: Long = 0,
                            val done: Long = 0) {
+
+    private val totalt = beskjed + innboks + oppgaver + done
+
     override fun toString(): String {
-        val totalt = beskjed + innboks + oppgaver + done
         return "NumberOfRecords(beskjed=$beskjed, innboks=$innboks, oppgaver=$oppgaver, done=$done, totalt=$totalt)"
     }
 
