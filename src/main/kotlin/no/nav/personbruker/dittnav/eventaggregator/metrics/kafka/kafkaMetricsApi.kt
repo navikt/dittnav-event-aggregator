edce69fde3f5ef8b119a396374d87a7afe660da4
Jørn Greger Nyegaard-Larsen <jorn.greger.nyegaard-larsen@nav.no>
2020-05-20 16:13:30 +0200

    Enderet til mer beskrivende variabelnavn.
    PB-438. Sammenligne antall kafka-eventer mot rader i cache-en

--- src/main/kotlin/no/nav/personbruker/dittnav/eventaggregator/metrics/kafka/kafkaMetricsApi.kt
+++ src/main/kotlin/no/nav/personbruker/dittnav/eventaggregator/metrics/kafka/kafkaMetricsApi.kt
@@ -9,32 +9,32 @@ import io.ktor.routing.get
 fun Routing.kafkaMetricsApi(eventCounterService: EventCounterService) {
 
     get("/internal/count/all") {
-        val result = eventCounterService.countAllEvents()
-        call.respondText(text = result.toString(), contentType = ContentType.Text.Plain)
+        val numberOfEvents = eventCounterService.countAllEvents()
+        call.respondText(text = numberOfEvents.toString(), contentType = ContentType.Text.Plain)
     }
 
     get("/internal/count/beskjed") {
-        val result = eventCounterService.countBeskjeder()
-        val response = "Antall beskjeder: $result"
-        call.respondText(text = response, contentType = ContentType.Text.Plain)
+        val numberOfEvents = eventCounterService.countBeskjeder()
+        val responseText = "Antall beskjeder: $numberOfEvents"
+        call.respondText(text = responseText, contentType = ContentType.Text.Plain)
     }
 
     get("/internal/count/innboks") {
-        val result = eventCounterService.countInnboksEventer()
-        val response = "Antall innboks-eventer: $result"
-        call.respondText(text = response, contentType = ContentType.Text.Plain)
+        val numberOfEvents = eventCounterService.countInnboksEventer()
+        val responseText = "Antall innboks-eventer: $numberOfEvents"
+        call.respondText(text = responseText, contentType = ContentType.Text.Plain)
     }
 
     get("/internal/count/oppgave") {
-        val result = eventCounterService.countOppgaver()
-        val response = "Antall oppgaver: $result"
-        call.respondText(text = response, contentType = ContentType.Text.Plain)
+        val numberOfEvents = eventCounterService.countOppgaver()
+        val responseText = "Antall oppgaver: $numberOfEvents"
+        call.respondText(text = responseText, contentType = ContentType.Text.Plain)
     }
 
     get("/internal/count/done") {
-        val result = eventCounterService.countDoneEvents()
-        val response = "Antall done-eventer: $result"
-        call.respondText(text = response, contentType = ContentType.Text.Plain)
+        val numberOfEvents = eventCounterService.countDoneEvents()
+        val responseText = "Antall done-eventer: $numberOfEvents"
+        call.respondText(text = responseText, contentType = ContentType.Text.Plain)
     }
 
 }
