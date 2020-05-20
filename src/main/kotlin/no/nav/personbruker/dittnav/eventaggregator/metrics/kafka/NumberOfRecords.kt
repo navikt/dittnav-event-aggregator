ff0f23f0345815988b2e9eb0b5b9c917479cca21
Jørn Greger Nyegaard-Larsen <jorn.greger.nyegaard-larsen@nav.no>
2020-05-20 13:53:33 +0200

    Tellerene settes til -1 for å verifisere at de faktisk får verdi på et senere tidspunkt.
    PB-438. Sammenligne antall kafka-eventer mot rader i cache-en

--- src/main/kotlin/no/nav/personbruker/dittnav/eventaggregator/metrics/kafka/NumberOfRecords.kt
+++ src/main/kotlin/no/nav/personbruker/dittnav/eventaggregator/metrics/kafka/NumberOfRecords.kt
@@ -1,9 +1,9 @@
 package no.nav.personbruker.dittnav.eventaggregator.metrics.kafka
 
-data class NumberOfRecords(var beskjed: Long = 0,
-                           var innboks: Long = 0,
-                           var oppgaver: Long = 0,
-                           var done: Long = 0) {
+data class NumberOfRecords(var beskjed: Long = -1,
+                           var innboks: Long = -1,
+                           var oppgaver: Long = -1,
+                           var done: Long = -1) {
     override fun toString(): String {
         val totalt = beskjed + innboks + oppgaver + done
         return "NumberOfRecords(beskjed=$beskjed, innboks=$innboks, oppgaver=$oppgaver, done=$done, totalt=$totalt)"
