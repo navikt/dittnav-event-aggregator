d61e74ac4e43a38ad8e9afb925334f50faf8610e
Jørn Greger Nyegaard-Larsen <jorn.greger.nyegaard-larsen@nav.no>
2020-08-05 16:29:51 +0200

    Sørger for at samme name scrubber brukes i hele appen. Dette er viktig siden hver name-scrubber-en har en cache, og denne cache-en ønsker vi bare en versjon av. For å oppdatere cache-en må det gjøres et database-kall, og dette behovet har nå blitt halvert.

--- src/main/kotlin/no/nav/personbruker/dittnav/eventaggregator/metrics/metricsProbeSetup.kt
+++ src/main/kotlin/no/nav/personbruker/dittnav/eventaggregator/metrics/metricsProbeSetup.kt
@@ -1,22 +1,17 @@
 package no.nav.personbruker.dittnav.eventaggregator.metrics
 
-import no.nav.personbruker.dittnav.eventaggregator.common.database.Database
 import no.nav.personbruker.dittnav.eventaggregator.config.Environment
 import no.nav.personbruker.dittnav.eventaggregator.metrics.db.DBMetricsProbe
 import no.nav.personbruker.dittnav.eventaggregator.metrics.influx.InfluxMetricsReporter
 import no.nav.personbruker.dittnav.eventaggregator.metrics.influx.SensuClient
 
-fun buildEventMetricsProbe(environment: Environment, database: Database): EventMetricsProbe {
+fun buildEventMetricsProbe(environment: Environment, nameScrubber: ProducerNameScrubber): EventMetricsProbe {
     val metricsReporter = resolveMetricsReporter(environment)
-    val nameResolver = ProducerNameResolver(database)
-    val nameScrubber = ProducerNameScrubber(nameResolver)
     return EventMetricsProbe(metricsReporter, nameScrubber)
 }
 
-fun buildDBMetricsProbe(environment: Environment, database: Database): DBMetricsProbe {
+fun buildDBMetricsProbe(environment: Environment, nameScrubber: ProducerNameScrubber): DBMetricsProbe {
     val metricsReporter = resolveMetricsReporter(environment)
-    val nameResolver = ProducerNameResolver(database)
-    val nameScrubber = ProducerNameScrubber(nameResolver)
     return DBMetricsProbe(metricsReporter, nameScrubber)
 }
 
