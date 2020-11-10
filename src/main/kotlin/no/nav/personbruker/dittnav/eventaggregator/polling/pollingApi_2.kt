968dde2a12aec5b65af15ce12d4aa85ce13fa6cf
Vanessa Kerr <Vanessa.kjolner.kerr@nav.no>
2020-11-10 21:38:06 +0100

    Fjerner kode som ikke brukes.

--- src/main/kotlin/no/nav/personbruker/dittnav/eventaggregator/polling/pollingApi_2.kt
+++
@@ -1,8 +0,0 @@
-package no.nav.personbruker.dittnav.eventaggregator.polling
-
-import io.ktor.routing.*
-import no.nav.personbruker.dittnav.eventaggregator.config.ApplicationContext
-
-fun Routing.pollingApi_2(appContext: ApplicationContext) {
-
-}
