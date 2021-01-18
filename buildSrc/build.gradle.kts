a3f91b642b2ad770c3257d21ed7e508137c04f50
Robert Larsen <robert.larsen@nav.no>
2021-01-18 16:12:48 +0100

    Siste versjon av dittnav-dependencies, med eksternVarsling i main.

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -7,7 +7,7 @@ repositories {
     maven("https://jitpack.io")
 }
 
-val dittNavDependenciesVersion = "2020.11.13-09.41-3725682edd36"
+val dittNavDependenciesVersion = "2021.01.18-15.40-485aeedf2f29"
 
 dependencies {
     implementation("com.github.navikt:dittnav-dependencies:$dittNavDependenciesVersion")
