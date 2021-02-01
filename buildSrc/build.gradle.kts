60d16506a094a6d06cff6cca939915ea712a1a68
Christopher Santana <christopher.santana@nav.no>
2021-02-01 13:24:28 +0100

    Bumpet dittnav-dependencies for å fikse SENSU_PORT.

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -7,7 +7,7 @@ repositories {
     maven("https://jitpack.io")
 }
 
-val dittNavDependenciesVersion = "2021.01.28-13.10-57381c88aaab"
+val dittNavDependenciesVersion = "2021.02.01-13.06-c4bc94f8b796"
 
 dependencies {
     implementation("com.github.navikt:dittnav-dependencies:$dittNavDependenciesVersion")
