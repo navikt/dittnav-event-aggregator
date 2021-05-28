02dc9ea53d70d24d5cb746a59cee1ba1a3060b99
Vanessa Kerr <Vanessa.kjolner.kerr@nav.no>
2021-05-28 08:46:26 +0200

    Fjerner kode som ikke blir brukt.
    feature/PB-726 leser fra Aiven topic

--- src/main/kotlin/no/nav/personbruker/dittnav/eventaggregator/config/Environment.kt
+++ src/main/kotlin/no/nav/personbruker/dittnav/eventaggregator/config/Environment.kt
@@ -3,9 +3,7 @@ package no.nav.personbruker.dittnav.eventaggregator.config
 import no.nav.personbruker.dittnav.common.util.config.IntEnvVar.getEnvVarAsInt
 import no.nav.personbruker.dittnav.common.util.config.StringEnvVar.getEnvVar
 
-data class Environment(//val bootstrapServers: String = getEnvVar("KAFKA_BOOTSTRAP_SERVERS"),
-                       //val schemaRegistryUrl: String = getEnvVar("KAFKA_SCHEMAREGISTRY_SERVERS"),
-                       val username: String = getEnvVar("SERVICEUSER_USERNAME"),
+data class Environment(val username: String = getEnvVar("SERVICEUSER_USERNAME"),
                        val password: String = getEnvVar("SERVICEUSER_PASSWORD"),
                        val groupId: String = getEnvVar("GROUP_ID"),
                        val dbHost: String = getEnvVar("DB_HOST"),
