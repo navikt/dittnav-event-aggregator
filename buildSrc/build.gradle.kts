a5e29b63bac248e6c651707c0be4f17b450fd0ea
Vanessa Kerr <Vanessa.kjolner.kerr@nav.no>
2021-05-31 09:58:05 +0200

    Fjerner nais fil som ikke blir brukt og oppdaterer dittnav-dependencies.
    feature/PB-727 gcp base

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -7,7 +7,7 @@ repositories {
     maven("https://jitpack.io")
 }
 
-val dittNavDependenciesVersion = "2021.03.18-15.31-4524f903fa47"
+val dittNavDependenciesVersion = "2021.05.21-08.32-f022ef6a77e9"
 
 dependencies {
     implementation("com.github.navikt:dittnav-dependencies:$dittNavDependenciesVersion")
