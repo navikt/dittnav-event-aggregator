9790b410196aa7b4657f6f3736e7cd3cba1dd3d1
Vanessa Kerr <Vanessa.kjolner.kerr@nav.no>
2021-06-18 08:42:48 +0200

    Prøver å få midlertidig tilgang så jeg kan gjøre INSERT i dev basen.

--- src/main/resources/db/migration/V1.1.8__Gir_tilgang_til_teamet.sql
+++ src/main/resources/db/migration/V1.1.8__Gir_tilgang_til_teamet.sql
@@ -5,6 +5,7 @@ $$
             (SELECT 1 from pg_roles where rolname = 'cloudsqliamuser')
         THEN
             GRANT USAGE ON SCHEMA public TO cloudsqliamuser;
+            GRANT USAGE ON SCHEMA "brukernotifikasjon-cache" TO cloudsqliamuser;
             GRANT ALL ON ALL TABLES IN SCHEMA public TO cloudsqliamuser;
             ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON TABLES TO cloudsqliamuser;
         END IF;
