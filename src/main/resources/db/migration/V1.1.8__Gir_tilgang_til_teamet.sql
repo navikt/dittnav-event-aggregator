963c699a5f7ee6f4e2d2c4d423aa0cbf146ca0ed
Vanessa Kerr <Vanessa.kjolner.kerr@nav.no>
2021-06-18 09:25:09 +0200

    La til flere tilganger. Prøver å få midlertidig tilgang så jeg kan gjøre INSERT i dev basen.

--- src/main/resources/db/migration/V1.1.8__Gir_tilgang_til_teamet.sql
+++ src/main/resources/db/migration/V1.1.8__Gir_tilgang_til_teamet.sql
@@ -5,11 +5,8 @@ $$
             (SELECT 1 from pg_roles where rolname = 'cloudsqliamuser')
         THEN
             GRANT USAGE ON SCHEMA public TO cloudsqliamuser;
-            GRANT USAGE ON SCHEMA "brukernotifikasjon-cache" TO cloudsqliamuser;
-            GRANT ALL ON ALL TABLES IN SCHEMA public TO cloudsqliamuser;
-            GRANT ALL ON ALL TABLES IN SCHEMA "brukernotifikasjon-cache" TO cloudsqliamuser;
-            ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON TABLES TO cloudsqliamuser;
-            ALTER DEFAULT PRIVILEGES IN SCHEMA "brukernotifikasjon-cache" GRANT ALL ON TABLES TO cloudsqliamuser;
+            GRANT SELECT ON ALL TABLES IN SCHEMA public TO cloudsqliamuser;
+            ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT ON TABLES TO cloudsqliamuser;
         END IF;
     END
 $$;
---
+++ src/main/resources/db/migration/V1.1.9__Gir_teamet_flere_rettigheter.sql
@@ -0,0 +1,15 @@
+DO
+$$
+    BEGIN
+        IF EXISTS
+            (SELECT 1 from pg_roles where rolname = 'cloudsqliamuser')
+        THEN
+            GRANT USAGE ON SCHEMA public TO cloudsqliamuser;
+            GRANT ALL ON ALL TABLES IN SCHEMA public TO cloudsqliamuser;
+            ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON TABLES TO cloudsqliamuser;
+            GRANT USAGE ON SCHEMA "brukernotifikasjon-cache" TO cloudsqliamuser;
+            GRANT ALL ON ALL TABLES IN SCHEMA "brukernotifikasjon-cache" TO cloudsqliamuser;
+            ALTER DEFAULT PRIVILEGES IN SCHEMA "brukernotifikasjon-cache" GRANT ALL ON TABLES TO cloudsqliamuser;
+        END IF;
+    END
+$$;
