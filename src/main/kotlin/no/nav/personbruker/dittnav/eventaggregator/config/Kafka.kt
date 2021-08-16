c95dcdd869402e58957661d04d422b55223d4f50
Christopher Santana <christopher.santana@nav.no>
2021-08-16 14:58:52 +0200

    Setter topic versjoner til v1.

--- src/main/kotlin/no/nav/personbruker/dittnav/eventaggregator/config/Kafka.kt
+++ src/main/kotlin/no/nav/personbruker/dittnav/eventaggregator/config/Kafka.kt
@@ -17,11 +17,11 @@ object Kafka {
 
     private val log: Logger = LoggerFactory.getLogger(Kafka::class.java)
 
-    val doneHovedTopicName = "min-side.privat-brukernotifikasjon-done-v3"
-    val beskjedHovedTopicName = "min-side.privat-brukernotifikasjon-beskjed-v4"
-    val innboksHovedTopicName = "min-side.privat-brukernotifikasjon-innboks-v3"
-    val oppgaveHovedTopicName = "min-side.privat-brukernotifikasjon-oppgave-v3"
-    val statusoppdateringHovedTopicName = "min-side.privat-brukernotifikasjon-statusoppdatering-v3"
+    val doneHovedTopicName = "min-side.privat-brukernotifikasjon-done-v1"
+    val beskjedHovedTopicName = "min-side.privat-brukernotifikasjon-beskjed-v1"
+    val innboksHovedTopicName = "min-side.privat-brukernotifikasjon-innboks-v1"
+    val oppgaveHovedTopicName = "min-side.privat-brukernotifikasjon-oppgave-v1"
+    val statusoppdateringHovedTopicName = "min-side.privat-brukernotifikasjon-statusoppdatering-v1"
 
     fun counterConsumerProps(env: Environment, eventTypeToConsume: EventType): Properties {
         val groupIdAndEventType = "dn-aggregator_metrics_counter_" + eventTypeToConsume.eventType
