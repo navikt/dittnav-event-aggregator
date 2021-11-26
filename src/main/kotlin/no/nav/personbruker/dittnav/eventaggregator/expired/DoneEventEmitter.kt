9ada1df5f18dee7dfe71050e798739a7148374ab
Mauricio Burgos Herrera <mauricio.burgos.herrera@nav.no>
2021-11-26 12:52:33 +0100

    First sketch implementation

---
+++ src/main/kotlin/no/nav/personbruker/dittnav/eventaggregator/expired/DoneEventEmitter.kt
@@ -0,0 +1,12 @@
+package no.nav.personbruker.dittnav.eventaggregator.expired
+
+import no.nav.personbruker.dittnav.eventaggregator.beskjed.Beskjed
+
+class DoneEventEmitter {
+
+
+    fun emittBeskjedDone(beskjeder: List<Beskjed>) {
+        TODO("Not yet implemented")
+    }
+
+}
---
+++ src/main/kotlin/no/nav/personbruker/dittnav/eventaggregator/expired/ExpiredPersistingService.kt
@@ -0,0 +1,10 @@
+package no.nav.personbruker.dittnav.eventaggregator.expired
+
+import no.nav.personbruker.dittnav.eventaggregator.beskjed.Beskjed
+
+class ExpiredPersistingService {
+    fun getExpiredNotifications(cursor: Int?): List<Beskjed> {
+        TODO("Implement")
+    }
+
+}
