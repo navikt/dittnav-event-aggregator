a859fcbb172d5226db3c5463a10c907c34e4a4b4
Atle Nordland <atle.nordland@nav.no>
2022-01-04 13:51:55 +0100

    Fikset ene testen som ikke fungerte med MockConsumer.
    MockConsumer fjerner records fra listen ved poll, så det går ikke å rulle offset tilbake.
    Tester derfor at seek har blitt kalt med riktig offset isteden.

--- src/intTest/kotlin/no/nav/personbruker/dittnav/eventaggregator/common/kafka/ConsumerTestIT.kt
+++ src/intTest/kotlin/no/nav/personbruker/dittnav/eventaggregator/common/kafka/ConsumerTestIT.kt
@@ -1,6 +1,9 @@
 package no.nav.personbruker.dittnav.eventaggregator.common.kafka
 
+import kotlinx.coroutines.TimeoutCancellationException
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withTimeout
 import no.nav.brukernotifikasjon.schemas.internal.BeskjedIntern
 import no.nav.brukernotifikasjon.schemas.internal.NokkelIntern
 import no.nav.personbruker.dittnav.eventaggregator.beskjed.AvroBeskjedObjectMother
@@ -8,19 +11,20 @@ import no.nav.personbruker.dittnav.eventaggregator.common.ThrowingEventCounterSe
 import no.nav.personbruker.dittnav.eventaggregator.common.exceptions.RetriableDatabaseException
 import no.nav.personbruker.dittnav.eventaggregator.common.exceptions.UnretriableDatabaseException
 import org.amshove.kluent.`should be equal to`
-import org.amshove.kluent.`should be greater than`
 import org.amshove.kluent.`should contain same`
+import org.amshove.kluent.invoking
+import org.amshove.kluent.shouldNotThrow
 import org.apache.kafka.clients.consumer.MockConsumer
+import org.apache.kafka.clients.consumer.OffsetAndMetadata
 import org.apache.kafka.clients.consumer.OffsetResetStrategy
 import org.apache.kafka.common.TopicPartition
-import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 
 class ConsumerTestIT {
 
     private val topicPartition = TopicPartition("topic", 0)
 
-    private fun consumerMock() = MockConsumer<NokkelIntern, BeskjedIntern>(OffsetResetStrategy.EARLIEST).also {
+    private fun mockConsumer() = MockConsumer<NokkelIntern, BeskjedIntern>(OffsetResetStrategy.EARLIEST).also {
         it.subscribe(listOf(topicPartition.topic()))
         it.rebalance(listOf(topicPartition))
         it.updateBeginningOffsets(mapOf(topicPartition to 0))
@@ -29,7 +33,7 @@ class ConsumerTestIT {
     @Test
     fun `Should attempt process each event exactly once if no exceptions are thrown`() {
 
-        val consumerMock = consumerMock()
+        val consumerMock = mockConsumer()
         val eventProcessor = ThrowingEventCounterService<BeskjedIntern>()
         val beskjedConsumer = Consumer(topicPartition.topic(), consumerMock, eventProcessor)
 
@@ -48,9 +52,8 @@ class ConsumerTestIT {
     }
 
     @Test
-    @Disabled
     fun `Should attempt to process some events multiple times if a retriable exception was raised`() {
-        val consumerMock = MockConsumer<NokkelIntern, BeskjedIntern>(OffsetResetStrategy.EARLIEST).also {
+        val consumerMock = MockConsumerWithRollbackCheck(1337L).also {
             it.subscribe(listOf(topicPartition.topic()))
             it.rebalance(listOf(topicPartition))
             it.updateBeginningOffsets(mapOf(topicPartition to 0))
@@ -58,29 +61,30 @@ class ConsumerTestIT {
 
         val beskjeder = createEventRecords(3, topicPartition, AvroBeskjedObjectMother::createBeskjed)
 
-        val eventProcessor =
-            ThrowingEventCounterService<BeskjedIntern>(RetriableDatabaseException("Transient error"), 2)
+        val eventProcessor = ThrowingEventCounterService<BeskjedIntern>(RetriableDatabaseException("Transient error"), 2)
         val beskjedConsumer = Consumer(topicPartition.topic(), consumerMock, eventProcessor)
 
-        runBlocking {
-            beskjedConsumer.startPolling()
+        invoking {
+            runBlocking {
+                beskjedConsumer.startPolling()
 
-            beskjeder.forEachIndexed { index, consumerRecord ->
-                consumerMock.addRecord(consumerRecord)
-                delayUntilDone(beskjedConsumer, index + 1)
+                beskjeder.forEach {
+                    consumerMock.addRecord(it)
+                }
+
+                withTimeout(1000) {
+                    while (!consumerMock.hasRollbacked) {
+                        delay(10)
+                    }
+                }
+                beskjedConsumer.stopPolling()
             }
-
-            beskjedConsumer.stopPolling()
-        }
-
-        eventProcessor.successfulEventsCounter `should be equal to` beskjeder.size
-        eventProcessor.invocationCounter `should be greater than` beskjeder.size
-        eventProcessor.successfulEvents `should contain same` beskjeder.map { it.value() }
+        } shouldNotThrow TimeoutCancellationException::class
     }
 
     @Test
     fun `Should stop processing events if a non-retriable exception was raised`() {
-        val consumerMock = consumerMock()
+        val consumerMock = mockConsumer()
         val eventProcessor = ThrowingEventCounterService<BeskjedIntern>(UnretriableDatabaseException("Fatal error"), 3)
         val beskjedConsumer = Consumer(topicPartition.topic(), consumerMock, eventProcessor)
 
@@ -94,5 +98,22 @@ class ConsumerTestIT {
 
         eventProcessor.successfulEventsCounter `should be equal to` 2
         eventProcessor.invocationCounter `should be equal to` 3
+    }
+
+    private class MockConsumerWithRollbackCheck(var dummyResetOffsetValue: Long) :
+        MockConsumer<NokkelIntern, BeskjedIntern>(OffsetResetStrategy.EARLIEST) {
+        //MockConsumer har problemer med seek, så må teste på litt hacky måte
+        var hasRollbacked = false
+        override fun seek(partition: TopicPartition?, offset: Long) {
+            if (offset == dummyResetOffsetValue) {
+                hasRollbacked = true
+                return
+            }
+            super.seek(partition, offset)
+        }
+
+        override fun committed(partitions: MutableSet<TopicPartition>?): MutableMap<TopicPartition, OffsetAndMetadata> {
+            return mutableMapOf(partitions!!.first() to OffsetAndMetadata(dummyResetOffsetValue))
+        }
     }
 }
