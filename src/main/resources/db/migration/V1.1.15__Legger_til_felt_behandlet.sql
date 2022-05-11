368cb9be46a62c6bcff1b58354b0e4159b1c0b79
Robert Larsen <robert.larsen@nav.no>
2022-05-11 08:47:44 +0200

    Fjerner migration som aldri har blitt kjørt.

--- src/main/resources/db/migration/V1.1.15__Legger_til_felt_behandlet.sql
+++ src/main/resources/db/migration/V1.1.15__Legger_til_felt_behandlet.sql
@@ -3,9 +3,3 @@ ALTER TABLE oppgave ADD COLUMN forstBehandlet timestamp without time zone;
 ALTER TABLE innboks ADD COLUMN forstBehandlet timestamp without time zone;
 ALTER TABLE statusoppdatering ADD COLUMN forstBehandlet timestamp without time zone;
 ALTER TABLE done ADD COLUMN forstBehandlet timestamp without time zone;
-
-update beskjed set forstBehandlet = case when eventtidspunkt < '1975-01-01' then to_timestamp(extract(epoch from eventtidspunkt) * 1000) at time zone 'UTC' else eventtidspunkt end;
-update oppgave set forstBehandlet = case when eventtidspunkt < '1975-01-01' then to_timestamp(extract(epoch from eventtidspunkt) * 1000) at time zone 'UTC' else eventtidspunkt end;
-update innboks set forstBehandlet = case when eventtidspunkt < '1975-01-01' then to_timestamp(extract(epoch from eventtidspunkt) * 1000) at time zone 'UTC' else eventtidspunkt end;
-update statusoppdatering set forstBehandlet = case when eventtidspunkt < '1975-01-01' then to_timestamp(extract(epoch from eventtidspunkt) * 1000) at time zone 'UTC' else eventtidspunkt end;
-update done set forstBehandlet = case when eventtidspunkt < '1975-01-01' then to_timestamp(extract(epoch from eventtidspunkt) * 1000) at time zone 'UTC' else eventtidspunkt end;
