a770a9620cbf7f4868937f400bad143ea7e76290
Christopher Santana <christopher.santana@nav.no>
2022-06-28 13:08:12 +0200

    Minifiks på navn på generics.

--- src/main/kotlin/no/nav/personbruker/dittnav/eventaggregator/common/EventBatchProcessorService.kt
+++ src/main/kotlin/no/nav/personbruker/dittnav/eventaggregator/common/EventBatchProcessorService.kt
@@ -4,16 +4,16 @@ import no.nav.brukernotifikasjon.schemas.internal.NokkelIntern
 import org.apache.kafka.clients.consumer.ConsumerRecord
 import org.apache.kafka.clients.consumer.ConsumerRecords
 
-interface EventBatchProcessorService<N, T> {
+interface EventBatchProcessorService<K, V> {
 
-    suspend fun processEvents(events: ConsumerRecords<N, T>)
+    suspend fun processEvents(events: ConsumerRecords<K, V>)
 
-    val ConsumerRecord<NokkelIntern, T>.namespace: String get() = key().getNamespace()
+    val ConsumerRecord<NokkelIntern, V>.namespace: String get() = key().getNamespace()
 
-    val ConsumerRecord<NokkelIntern, T>.appnavn: String get() = key().getAppnavn()
+    val ConsumerRecord<NokkelIntern, V>.appnavn: String get() = key().getAppnavn()
 
-    val ConsumerRecord<NokkelIntern, T>.systembruker: String get() = key().getSystembruker()
+    val ConsumerRecord<NokkelIntern, V>.systembruker: String get() = key().getSystembruker()
 
-    val ConsumerRecord<NokkelIntern, T>.eventId: String get() = key().getEventId()
+    val ConsumerRecord<NokkelIntern, V>.eventId: String get() = key().getEventId()
 
 }
