8ca8a913e75434aed6cc377e385c802a4cbdae8f
Christopher Santana <christopher.santana@nav.no>
2022-08-08 09:36:07 +0200

    Bumper felles dependencies for å fikse sql-injection vulnerability.

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -7,7 +7,7 @@ repositories {
     maven("https://jitpack.io")
 }
 
-val dittNavDependenciesVersion = "2022.03.07-09.11-113dabef3447"
+val dittNavDependenciesVersion = "2022.08.08-08.46-1361f6529507"
 
 dependencies {
     implementation("com.github.navikt:dittnav-dependencies:$dittNavDependenciesVersion")
