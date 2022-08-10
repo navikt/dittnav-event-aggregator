8dc82c5b1baa5fb13f3f86be630975949e7bab52
Atle Nordland <atle.nordland@nav.no>
2022-08-10 13:44:21 +0200

    Lagre beskjed med R&R

--- src/main/kotlin/no/nav/personbruker/dittnav/eventaggregator/varsel/BeskjedSink.kt
+++ src/main/kotlin/no/nav/personbruker/dittnav/eventaggregator/varsel/BeskjedSink.kt
@@ -1,21 +1,73 @@
 package no.nav.personbruker.dittnav.eventaggregator.varsel
 
+import kotlinx.coroutines.runBlocking
 import no.nav.helse.rapids_rivers.JsonMessage
 import no.nav.helse.rapids_rivers.MessageContext
+import no.nav.helse.rapids_rivers.MessageProblems
 import no.nav.helse.rapids_rivers.RapidsConnection
 import no.nav.helse.rapids_rivers.River
+import no.nav.helse.rapids_rivers.asLocalDateTime
+import no.nav.helse.rapids_rivers.asOptionalLocalDateTime
+import no.nav.personbruker.dittnav.eventaggregator.beskjed.Beskjed
 import no.nav.personbruker.dittnav.eventaggregator.beskjed.BeskjedRepository
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 
-internal class BeskjedSink(rapidsConnection: RapidsConnection, beskjedRepository: BeskjedRepository): River.PacketListener {
+internal class BeskjedSink(rapidsConnection: RapidsConnection, private val beskjedRepository: BeskjedRepository) :
+    River.PacketListener {
+
+    private val log: Logger = LoggerFactory.getLogger(BeskjedSink::class.java)
 
     init {
         River(rapidsConnection).apply {
             validate { it.demandValue("@event_name", "beskjed") }
-            validate { it.requireKey("fodselsnummer") }
+            validate { it.requireKey(
+                "id",
+                "systembruker",
+                "namespace",
+                "appnavn",
+                "eventId",
+                "eventTidspunkt",
+                "forstBehandlet",
+                "fodselsnummer",
+                "grupperingsId",
+                "tekst",
+                "link",
+                "sikkerhetsnivaa",
+                "sistOppdatert",
+                "aktiv",
+                "eksternVarsling"
+            ) }
+            validate { it.interestedIn("synligFremTil", "prefererteKanaler")}
         }.register(this)
     }
 
     override fun onPacket(packet: JsonMessage, context: MessageContext) {
+        val beskjed = Beskjed(
+            id = packet["id"].intValue(),
+            systembruker = packet["systembruker"].textValue(),
+            namespace = packet["namespace"].textValue(),
+            appnavn = packet["appnavn"].textValue(),
+            eventId = packet["eventId"].textValue(),
+            eventTidspunkt = packet["eventTidspunkt"].asLocalDateTime(),
+            forstBehandlet = packet["forstBehandlet"].asLocalDateTime(),
+            fodselsnummer = packet["fodselsnummer"].textValue(),
+            grupperingsId = packet["grupperingsId"].textValue(),
+            tekst = packet["tekst"].textValue(),
+            link = packet["link"].textValue(),
+            sikkerhetsnivaa = packet["sikkerhetsnivaa"].intValue(),
+            sistOppdatert = packet["sistOppdatert"].asLocalDateTime(),
+            synligFremTil = packet["synligFremTil"].asOptionalLocalDateTime(),
+            aktiv = packet["aktiv"].booleanValue(),
+            eksternVarsling = packet["eksternVarsling"].booleanValue(),
+        )
 
+        runBlocking {
+            beskjedRepository.createInOneBatch(listOf(beskjed))
+        }
+    }
+
+    override fun onError(problems: MessageProblems, context: MessageContext) {
+        log.error(problems.toString())
     }
 }
--- src/test/kotlin/no/nav/personbruker/dittnav/eventaggregator/varsel/BeskjedSinkTest.kt
+++ src/test/kotlin/no/nav/personbruker/dittnav/eventaggregator/varsel/BeskjedSinkTest.kt
@@ -11,27 +11,42 @@ import no.nav.personbruker.dittnav.eventaggregator.common.database.util.list
 import org.junit.jupiter.api.Test
 
 class BeskjedSinkTest {
-    private val testRapid = TestRapid()
 
     private val database = LocalPostgresDatabase.migratedDb()
     private val beskjedRepository = BeskjedRepository(database)
 
-
     @Test
-    fun `test `() {
-        val beskjedSink = BeskjedSink(testRapid, beskjedRepository)
+    fun `lagre beskjed`() = runBlocking {
+        val testRapid = TestRapid()
+        BeskjedSink(testRapid, beskjedRepository)
 
+        testRapid.sendTestMessage(beskjedJson)
 
-        //testRapid.sendTestMessage()
-        runBlocking {
-            val beskjeder = getBeskjeder()
-
-            beskjeder.size shouldBe 0
-        }
+        val beskjeder = getBeskjeder()
+        beskjeder.size shouldBe 1
     }
-
 
     private suspend fun getBeskjeder(): List<Beskjed> {
         return database.dbQuery { this.prepareStatement("select * from beskjed").executeQuery().list { toBeskjed() } }
     }
+
+    private val beskjedJson = """{
+    "@event_name": "beskjed",
+    "id": "",
+    "systembruker": "",
+    "namespace": "",
+    "appnavn": "",
+    "eventId": "",
+    "eventTidspunkt": "2022-01-01T00:00:00",
+    "forstBehandlet": "2022-02-01T00:00:00",
+    "fodselsnummer": "",
+    "grupperingsId": "",
+    "tekst": "",
+    "link": "",
+    "sikkerhetsnivaa": 4,
+    "sistOppdatert": "2022-03-01T00:00:00",
+    "synligFremTil": "2022-04-01T00:00:00",
+    "aktiv": true,
+    "eksternVarsling": false
+    }""".trimIndent()
 }
