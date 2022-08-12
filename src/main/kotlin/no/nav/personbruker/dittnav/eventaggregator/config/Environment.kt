b10790289fe71b132262d17190cb8373ff808556
Atle Nordland <atle.nordland@nav.no>
2022-08-12 13:47:04 +0200

    rapidconfig og feature toggle

--- src/main/kotlin/no/nav/personbruker/dittnav/eventaggregator/config/Environment.kt
+++ src/main/kotlin/no/nav/personbruker/dittnav/eventaggregator/config/Environment.kt
@@ -31,9 +31,18 @@ data class Environment(val username: String = getEnvVar("SERVICEUSER_USERNAME"),
                        val doneInternTopicName: String = getEnvVar("INTERN_DONE_TOPIC"),
                        val doneInputTopicName: String = getEnvVar("INPUT_DONE_TOPIC"),
                        val doknotifikasjonStatusGroupId: String = getEnvVar("GROUP_ID_DOKNOTIFIKASJON_STATUS"),
-                       val doknotifikasjonStatusTopicName: String = getEnvVar("DOKNOTIFIKASJON_STATUS_TOPIC")
-
-)
+                       val doknotifikasjonStatusTopicName: String = getEnvVar("DOKNOTIFIKASJON_STATUS_TOPIC"),
+                       val rapidTopic: String = getEnvVar("RAPID_TOPIC"),
+                       val rapidEnabled: Boolean = getEnvVar("RAPID_ENABLED", "false").toBoolean()
+) {
+    fun rapidConfig(): Map<String, String> = mapOf(
+        "KAFKA_BOOTSTRAP_SERVERS" to aivenBrokers,
+        "KAFKA_CONSUMER_GROUP_ID" to "dittnav-event-aggregator-v1",
+        "KAFKA_RAPID_TOPIC" to rapidTopic,
+        "KAFKA_KEYSTORE_PATH" to securityConfig.variables!!.aivenKeystorePath,
+        "KAFKA_KEYSTORE_PASSWORD" to securityConfig.variables.aivenCredstorePassword,
+    )
+}
 
 data class SecurityConfig(
         val enabled: Boolean,
--- src/main/kotlin/no/nav/personbruker/dittnav/eventaggregator/config/bootstrap.kt
+++ src/main/kotlin/no/nav/personbruker/dittnav/eventaggregator/config/bootstrap.kt
@@ -8,9 +8,11 @@ import io.ktor.features.DefaultHeaders
 import io.ktor.routing.routing
 import io.prometheus.client.hotspot.DefaultExports
 import kotlinx.coroutines.runBlocking
+import no.nav.helse.rapids_rivers.RapidApplication
 import no.nav.personbruker.dittnav.eventaggregator.common.kafka.polling.pollingApi
 import no.nav.personbruker.dittnav.eventaggregator.done.waitTableApi
 import no.nav.personbruker.dittnav.eventaggregator.health.healthApi
+import no.nav.personbruker.dittnav.eventaggregator.varsel.BeskjedSink
 
 fun Application.mainModule(appContext: ApplicationContext = ApplicationContext()) {
     DefaultExports.initialize()
@@ -28,6 +30,11 @@ fun Application.mainModule(appContext: ApplicationContext = ApplicationContext()
 private fun Application.configureStartupHook(appContext: ApplicationContext) {
     environment.monitor.subscribe(ApplicationStarted) {
         Flyway.runFlywayMigrations(appContext.environment)
+        if(appContext.environment.rapidEnabled) {
+            RapidApplication.create(appContext.environment.rapidConfig()).apply {
+                BeskjedSink(this, appContext.beskjedRepository)
+            }.start()
+        }
         KafkaConsumerSetup.startAllKafkaPollers(appContext)
         appContext.periodicDoneEventWaitingTableProcessor.start()
         appContext.periodicConsumerPollingCheck.start()
