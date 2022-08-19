3614877796ca218fa482976d2e7b1bcc8390bab8
Atle Nordland <atle.nordland@nav.no>
2022-08-19 14:25:41 +0200

    Slutt å lese beskjed-felter som ikke er i bruk

--- src/main/kotlin/no/nav/personbruker/dittnav/eventaggregator/varsel/BeskjedSink.kt
+++ src/main/kotlin/no/nav/personbruker/dittnav/eventaggregator/varsel/BeskjedSink.kt
@@ -25,14 +25,11 @@ internal class BeskjedSink(rapidsConnection: RapidsConnection, private val beskj
             validate { it.demandValue("@event_name", "beskjed") }
             validate { it.demandValue("aktiv", true) }
             validate { it.requireKey(
-                "systembruker",
                 "namespace",
                 "appnavn",
                 "eventId",
-                "eventTidspunkt",
                 "forstBehandlet",
                 "fodselsnummer",
-                "grupperingsId",
                 "tekst",
                 "link",
                 "sikkerhetsnivaa",
@@ -45,14 +42,14 @@ internal class BeskjedSink(rapidsConnection: RapidsConnection, private val beskj
     override fun onPacket(packet: JsonMessage, context: MessageContext) {
         val beskjed = Beskjed(
             id = null,
-            systembruker = packet["systembruker"].textValue(),
+            systembruker = "N/A",
             namespace = packet["namespace"].textValue(),
             appnavn = packet["appnavn"].textValue(),
             eventId = packet["eventId"].textValue(),
-            eventTidspunkt = packet["eventTidspunkt"].asLocalDateTime(),
+            eventTidspunkt = packet["forstBehandlet"].asLocalDateTime(), //Felt ikke i bruk
             forstBehandlet = packet["forstBehandlet"].asLocalDateTime(),
             fodselsnummer = packet["fodselsnummer"].textValue(),
-            grupperingsId = packet["grupperingsId"].textValue(),
+            grupperingsId = "N/A",
             tekst = packet["tekst"].textValue(),
             link = packet["link"].textValue(),
             sikkerhetsnivaa = packet["sikkerhetsnivaa"].intValue(),
