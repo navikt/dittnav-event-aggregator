1d44a2f2e94ff3bf193e546584b4cb4bd01b5b1b
Atle Nordland <atle.nordland@nav.no>
2022-09-30 10:29:18 +0200

    Revert "Ha med dummy-tag i metrics for å ikke overskrive forrige"
    This reverts commit f027219a2e18bbcb03ff88ff3397364b3ad7738c.

--- src/main/kotlin/no/nav/personbruker/dittnav/eventaggregator/varsel/RapidMetricsProbe.kt
+++ src/main/kotlin/no/nav/personbruker/dittnav/eventaggregator/varsel/RapidMetricsProbe.kt
@@ -6,15 +6,6 @@ import no.nav.personbruker.dittnav.eventaggregator.metrics.KAFKA_RAPID_EVENTS_PR
 
 class RapidMetricsProbe(private val metricsReporter: MetricsReporter) {
 
-    companion object {
-        private var dummyTag = 0
-
-        private fun nextDummyTag(): String {
-            if(dummyTag == 19) dummyTag = 0 else dummyTag +=1
-            return dummyTag.toString()
-        }
-    }
-
     suspend fun countProcessed(eventType: EventType, producerApp: String) {
         metricsReporter.registerDataPoint(KAFKA_RAPID_EVENTS_PROCESSED, counterField(1), createTagMap(eventType.name, producerApp))
     }
@@ -22,5 +13,5 @@ class RapidMetricsProbe(private val metricsReporter: MetricsReporter) {
     private fun counterField(events: Int): Map<String, Int> = listOf("counter" to events).toMap()
 
     private fun createTagMap(eventType: String, producer: String): Map<String, String> =
-        listOf("eventType" to eventType, "producer" to producer, "dummytag" to nextDummyTag()).toMap()
+        listOf("eventType" to eventType, "producer" to producer).toMap()
 }
